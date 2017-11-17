94f3c0ecc8615807d27a7fe6ca4349d3c77882b3
Yaroslav Mayevsky <leviaphan.ua@gmail.com>
2017-11-17 10:04:50 +0200

    add script to setUp git aliases

---
+++ setUpGitAliases.ps1
@@ -0,0 +1,7 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.s status
+git config --global alias.f fetch
+git config --global alias.m 'merge --no-ff'
+git config --global alias.last 'log -1 HEAD'
